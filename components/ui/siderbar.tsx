2b01e84043bda8630487ad4895a6887938259fb2
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-02-19 14:05:28 +0100

    add siderbar component

---
+++ components/ui/siderbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <section className='bg-amber-950'>
+    <header>
+        <h1 className='text-white'>Future Store</h1>
+        <span>Admin Panel</span>
+    </header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Contact</li>
+        </ul>
+
+    </nav>
+    </section>
+  )
+}
