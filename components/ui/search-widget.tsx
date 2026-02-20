480f1cc1264d015359579927b6c21c2dcff22cc3
Velvet <velvet.pinkish@gmail.com>
2026-02-20 10:30:35 +0100

    Create search input field

--- components/ui/search-widget.tsx
+++ components/ui/search-widget.tsx
@@ -1,7 +1,19 @@
 'use client'
 
-import { Funnel } from 'lucide-react';
+import { Search, Funnel } from 'lucide-react';
 
 export default function SearchWidget() {
-    
+    return(
+        <section className="flex p-3">
+            
+            <div className="flex gap-3 relative w-120 p-2 border-1 border-gray-300 rounded-xl">
+                <Search className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none" />
+                <input
+                    type="text"
+                    placeholder="Search products..."
+                    className="w-full pl-10"
+                />
+            </div>
+        </section>
+    )
 }
