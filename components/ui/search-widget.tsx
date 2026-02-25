ea5f53b4df7310c81d632a3e5eb4a0490d168f0e
Velvet <velvet.pinkish@gmail.com>
2026-02-25 14:42:38 +0100

    Style widget and add focus effect

--- components/ui/search-widget.tsx
+++ components/ui/search-widget.tsx
@@ -4,26 +4,26 @@ import { ChevronDown, Funnel, Search } from 'lucide-react';
 
 export default function SearchWidget() {
     return(
-        <section className="flex p-3 gap-10 border border-gray-100 items-center">
+        <section className="flex bg-white rounded-2xl p-3 gap-10 border border-gray-300 items-center">
             
-            <div className="flex gap-3 relative w-120 p-1.5 border border-gray-300 rounded-xl">
+            <div className="flex gap-3 relative w-full p-1.5 border-2 border-gray-300 rounded-xl focus-within:border-blue-500 focus-within:border-2">
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none" />
                 <input
                     type="text"
                     placeholder="Search products..."
-                    className="w-full pl-10"
+                    className="w-full pl-10 focus:outline-none"
                 />
             </div>
 
             <div className="flex p-1.5 border border-gray-300 rounded-xl">
-                <button className="flex gap-4 items-center" type="button">
+                <button className="flex whitespace-nowrap gap-4 items-center" type="button">
                     All categories
                     <ChevronDown />
                 </button>
             </div>
 
             <div className="flex p-1.5 border border-gray-300 rounded-xl">
-                <button className="flex gap-4 items-center" type="button">
+                <button className="flex whitespace-nowrap gap-4 items-center" type="button">
                     All status
                     <ChevronDown />
                 </button>
