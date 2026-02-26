fcdb5ab847e6586243a08bbc93130183927928bb
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-02-26 11:31:40 +0100

    added padding to filters and focus color on search

--- components/ui/search-widget.tsx
+++ components/ui/search-widget.tsx
@@ -26,7 +26,7 @@ export default function SearchWidget() {
 
   return (
     <section className="flex bg-white rounded-2xl p-3 gap-10 border border-gray-300 items-center">
-      <div className="flex gap-3 relative w-full p-1.5 border-2 border-gray-300 rounded-xl focus-within:border-blue-500 focus-within:border-2">
+      <div className="flex gap-3 relative w-full p-1.5 border border-gray-300 rounded-xl focus-within:border-purple-700 focus-within:border">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none" />
         <input
           type="text"
@@ -40,7 +40,7 @@ export default function SearchWidget() {
 
       <div className="flex p-1.5 border border-gray-300 rounded-xl">
         <button
-          className="flex whitespace-nowrap gap-4 items-center"
+          className="flex whitespace-nowrap gap-4 px-2 items-center"
           type="button"
         >
           All categories
@@ -50,7 +50,7 @@ export default function SearchWidget() {
 
       <div className="flex p-1.5 border border-gray-300 rounded-xl">
         <button
-          className="flex whitespace-nowrap gap-4 items-center"
+          className="flex whitespace-nowrap gap-4 px-2 items-center"
           type="button"
         >
           All status
