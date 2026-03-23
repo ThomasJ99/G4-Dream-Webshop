a1384d6bfe59a3d3a81d335b556cff945b856590
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-23 16:59:29 +0100

    Fix img tag to Image on hero, tweaked text styling and Link styling

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,51 +6,40 @@ export default function Hero() {
   return (
     <header className="relative min-h-[70vh] flex items-center">
       <div className="absolute inset-0">
-        <img
+        <Image
           src={
             "https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=1920&q=80"
           }
           alt="Dream Webshop hero image"
-          fill="true"
+          fill
           className="object-cover"
           // Make it load fast when changed to Image
         />
+        {/* Gradient looks pretty bad in mobile view */}
         <div className="absolute inset-0 bg-linear-to-r from-white/95 via-white/70 to-transparent"></div>
       </div>
 
       {/* Text section */}
       <section className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-20 lg:py-32 w-full">
-        <span className="text-sm font-semibold text-blue-600 uppercase tracking-wide mb-4">
-          Spring & Summer 2026
-        </span>
-        
-        <h1 className="font-serif text-4xl sm:text-5xl lg:text-6xl font-medium tracking-tight text-balance leading-tight">
-          Discover your personal style
-        </h1>
+        <div className="max-w-lg">
+          <span className="text-sm font-semibold text-blue-600 uppercase tracking-wide mb-4">
+            Spring & Summer 2026
+          </span>
 
-        <p>
-          Timeless fashion and accessories — carefully curated for those who
-          appreciate quality and design.
-        </p>
+          <h1 className="font-serif text-4xl sm:text-5xl lg:text-6xl font-medium tracking-tight text- leading-tight">
+            Discover your personal style
+          </h1>
 
-        <div className="bg-blue-600 hover:bg-blue-700 text-white inline-block py-3 px-5 rounded-lg cursor-pointer">
-          <div className="flex gap-2">
-            <Link href={"/products"}>Explore collection</Link>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              aria-hidden="true"
-            >
-              <path d="M5 12h14"></path>
-              <path d="m12 5 7 7-7 7"></path>
-            </svg>
+          <p className="mt-6 text-lg text-black/70 max-w-md leading-relaxed">
+            Timeless fashion and accessories — carefully curated for those who
+            appreciate quality and design.
+          </p>
+
+          <div className="bg-blue-600 text-center hover:bg-blue-700 text-white inline-block py-2.5 px-5 mt-8 rounded-lg cursor-pointer">
+            <div className="flex gap-2">
+              <Link href={"/products"}>Explore collection</Link>
+              <ArrowRight className="ml-2 mt-1 size-4" />
+            </div>
           </div>
         </div>
       </section>
