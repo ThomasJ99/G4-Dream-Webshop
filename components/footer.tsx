7c3ce510b81cb8668c91daf8ff2f3e6c92ef9e57
Alex <olofalexander.larsson@gmail.com>
2026-03-24 09:42:48 +0100

    1

---
+++ components/footer.tsx
@@ -0,0 +1,41 @@
+export default function Footer() {
+  return (
+    <footer className=" shadow-[0_-1px_3px_rgba(0,0,0,0.1)] w-full h-14 mt-20">
+      <div className="bg-red-500 flex ">
+        <div>
+          <h3>DreamShop</h3>
+          <p></p>
+        </div>
+
+        <div></div>
+
+        <div></div>
+
+        <div></div>
+      </div>
+    </footer>
+  );
+}
+
+// NORD
+// Scandinavian fashion for modern living. Timeless design meets sustainable craftsmanship.
+
+// Shop
+// All Products
+// Outerwear
+// Knitwear
+// Accessories
+
+// Support
+// Contact Us
+// Shipping & Returns
+// Size Guide
+// FAQ
+
+// Newsletter
+// Subscribe for updates on new arrivals and exclusive offers.
+
+// input:
+// Your email
+// button:
+// Join
