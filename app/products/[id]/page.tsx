648ecae860bcad50ba6491f19b5a31b6ca2cb2c4
4pooled <edchallis@protonmail.com>
2026-03-24 14:04:18 +0100

    added more html data

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { API_URL } from "@/lib/config";
 
 export default async function ProductDetail({
@@ -11,15 +12,26 @@ export default async function ProductDetail({
 
   console.log(Object.keys(data));
 
+  console.log(data.tags);
+
   if (!Object.keys(data).length) {
-    return <h1>product not found lol</h1>;
+    return <h1>product not found</h1>;
   }
 
   return (
-    <div>
+    <main>
+      <Image
+	src={data.thumbnail}
+	width={100}
+	height={100}
+	alt=""
+      />
+
       <h1>id: {data.id}</h1>
       <p>category: {data.categoryId}</p>
+      <p>description: {data.description}</p>
       <p>title: {data.title}</p>
-    </div>
+      <p>price: {data.price}</p>
+    </main>
   );
 }
