3aba73af0aad7f1c9d5f55d316f0f99211cad5d1
4pooled <edchallis@protonmail.com>
2026-03-25 09:33:27 +0100

    added some tailwind, still wip

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -10,28 +10,47 @@ export default async function ProductDetail({
     res.json(),
   );
 
-  console.log(Object.keys(data));
-
-  console.log(data.tags);
-
   if (!Object.keys(data).length) {
     return <h1>product not found</h1>;
   }
 
-  return (
-    <main>
-      <Image
-	src={data.thumbnail}
-	width={100}
-	height={100}
-	alt=""
-      />
+  const ProductElement = () => {
+    const productTags = data.tags.join(", ");
 
-      <h1>id: {data.id}</h1>
-      <p>category: {data.categoryId}</p>
-      <p>description: {data.description}</p>
-      <p>title: {data.title}</p>
-      <p>price: {data.price}</p>
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="aspect-[3/4] relative rounded-lg overflow-hidden bg-secondary">
+          <Image
+            className="object-cover"
+            src={data.images[0]}
+            width={500}
+            height={500}
+            alt=""
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <div>
+	    <p className="text-sm text-muted-foreground uppercase tracking-wide mb-2">{productTags}</p>
+            <h1 className="font-serif text-3xl sm:text-4xl font-medium mb-4">
+              {data.title}
+            </h1>
+	    <p className="text-2xl font-medium">{data.price} kr</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+      <a
+        className="text-sm text-muted-foreground hover:text-foreground transition-colors"
+        href="/products"
+      >
+        Back to Products
+      </a>
+      <ProductElement />
     </main>
   );
 }
