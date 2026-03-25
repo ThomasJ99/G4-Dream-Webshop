3aa62d5396f84478559b83ffa63fe3de888b68ba
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-25 11:41:18 +0100

    Add route to /cart which will house the checkout section

---
+++ app/cart/page.tsx
@@ -0,0 +1,15 @@
+export default function Cart () {
+    return (
+        <main>
+            {/* If no products */}
+            <section>
+no
+            </section>
+
+            {/* If products */}
+            <section>
+yes
+            </section>
+        </main>
+    )
+}
