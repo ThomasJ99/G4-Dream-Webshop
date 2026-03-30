32a474079b543933b1ba426be5be301e990e6606
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-30 13:55:47 +0200

    Fix product grid animation to have a delay between each product

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -4,16 +4,15 @@ import { ProductCard } from "./product-card";
 interface ProductCardProps {
   products: Product[];
   categories: Category[];
-  index?: number;
 }
 
-export default function ProductGrid({ products, categories, index = 0 }: ProductCardProps) {
+export default function ProductGrid({ products, categories }: ProductCardProps) {
   console.log(products[0]);
   return (
     <div>
       {products.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
-          {products.map((product) => {
+          {products.map((product, index) => {
             const category = categories.find((category) => category.id === product.categoryId);
 
             return <ProductCard key={product.id} product={product} category={category} index={index} />;
