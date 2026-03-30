88684ba27039003fd7be62430e1ec298372a6d6c
Alex <olofalexander.larsson@gmail.com>
2026-03-30 10:47:42 +0200

    Color on star

--- components/ui/add-favorite.tsx
+++ components/ui/add-favorite.tsx
@@ -1,20 +1,25 @@
 "use client";
 import { Star } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./button";
 
 export default function AddFavorite({ productID }: { productID: number }) {
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
     if (!productID) {
       console.error("Missing productID");
       return;
     }
+
     console.log(productID);
   };
   return (
-    <Button className="w-fit group hover:cursor-pointer" onClick={handleClick}>
+    <Button className="w-fit" onClick={() => setClicked((c) => !c)}>
       <Star
-        className="transition-transform duration-1000 group-hover:scale-110 group-hover:rotate-[360deg]"
-        fill="white"
+        className={`transition-transform duration-500 ${clicked ? "scale-110 rotate-[360deg]" : ""}`}
+        fill={clicked ? "gold" : "white"}
+        stroke={clicked ? "gold" : "white"}
       />
     </Button>
   );
