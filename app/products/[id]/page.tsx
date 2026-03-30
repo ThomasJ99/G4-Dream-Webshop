51d3bf67a2a5373dcbee5f1e18468d9f9e581924
victor <victnek@gmail.com>
2026-03-30 12:12:52 +0200

    Add addToCart action

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Form from "next/form";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import { getCartAction } from "@/lib/actions/cart-actions";
+import { addToCart } from "@/lib/actions/cart-actions";
 import { getProductById } from "@/lib/db/products-db";
 import type { Product } from "@/lib/types";
 
@@ -48,7 +48,8 @@ export default async function ProductPage({
           </p>
 
           {/* add to cart */}
-          <Form action={getCartAction}>
+          <Form action={addToCart}>
+            <input type="hidden" name="product_id" value={product.id}></input>
             <button
               type="submit"
               className="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-primary text-primary-foreground hover:bg-primary/90 h-10 rounded-md px-6 has-[>svg]:px-4 w-full"
--- lib/actions/cart-actions.ts
+++ lib/actions/cart-actions.ts
@@ -1,5 +1,6 @@
 "use server";
 import { cookies } from "next/headers";
+import { supabase } from "@/supabaseClient";
 import { createCart } from "../db/carts-db";
 
 export const getCartAction = async () => {
@@ -7,12 +8,13 @@ export const getCartAction = async () => {
 
   const cookieStore = await cookies();
   const cartCookie = cookieStore.get("cartId");
+  let cartId = cartCookie?.value;
 
   console.log(cartCookie);
   if (!cartCookie) {
     const cart = await createCart();
     cookieStore.set("cartId", cart.id);
-    console.log(cart);
+    cartId = cart.id;
   }
 
   //   cookieStore.set("cartId", "1", {
@@ -21,3 +23,53 @@ export const getCartAction = async () => {
   //     maxAge: 60 * 60 * 24 * 30, // 30 days
   //   });
 };
+
+export async function addToCart(formData: FormData) {
+  console.log("hej");
+
+  const cookieStore = await cookies();
+  const cartCookie = cookieStore.get("cartId");
+  let cartId = cartCookie?.value;
+
+  console.log(cartCookie);
+  if (!cartCookie) {
+    const cart = await createCart();
+    cookieStore.set("cartId", cart.id);
+    cartId = cart.id;
+  }
+
+  const product_id = formData.get("product_id") as string;
+  //const user_id = 'demo-user'; // byt mot auth senare
+  const cart_id = cartId;
+
+  // 1. Kolla om produkten redan finns
+  const { data: existingItem } = await supabase
+    .from("cart_items")
+    .select("*")
+    .eq("cart_id", cart_id)
+    .eq("product_id", product_id)
+    .single();
+
+  if (existingItem) {
+    // 2. Uppdatera quantity
+    await supabase
+      .from("cart_items")
+      .update({
+        quantity: existingItem.quantity + 1,
+      })
+      .eq("id", existingItem.id);
+  }
+  // else if(user_id) {await supabase.from("cart_items").insert({
+  //     user_id,
+  //     product_id,
+  //     quantity: 1,
+  //   });}
+  else {
+    // 3. Skapa ny rad
+    await supabase.from("cart_items").insert({
+      cart_id,
+      product_id,
+      quantity: 1,
+    });
+  }
+}
