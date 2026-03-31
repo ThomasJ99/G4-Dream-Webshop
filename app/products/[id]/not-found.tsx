a8170d831748a285577de6898d9a887e615139ce
4pooled <edchallis@protonmail.com>
2026-03-31 20:08:15 +0200

    added same styling to header in not-found.tsx as page.tsx

--- app/products/[id]/not-found.tsx
+++ app/products/[id]/not-found.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 export default async function NotFound() {
   return (
-    <main className="p-8">
-      <Link
-        href={"/products"}
-        className="inline-flex items-center text-sm text-muted-foreground hover:text-foreground transition-colors mb-8"
-      >
-        <ChevronLeft className="h-4 w-4 mr-1" />
-        Back to Products
-      </Link>
+    <main>
+      <header className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+        <Link
+          href={"/products"}
+          className="inline-flex items-center text-sm text-muted-foreground hover:text-foreground transition-colors"
+        >
+          <ChevronLeft className="h-4 w-4 mr-1" />
+          Back to Products
+        </Link>
+      </header>
 
       <section className="text-center py-16">
         <CircleX className="h-16 w-16 mx-auto text-muted-foreground" />
