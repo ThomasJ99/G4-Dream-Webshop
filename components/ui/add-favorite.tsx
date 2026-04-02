cd2e5306f72574b435d160e3892eaa3b60256864
Alex <olofalexander.larsson@gmail.com>
2026-04-02 11:45:13 +0200

    Removed unused code.

--- components/ui/add-favorite.tsx
+++ components/ui/add-favorite.tsx
@@ -1,35 +1,3 @@
-// "use client";
-// import { Star } from "lucide-react";
-// import { API_URL } from "@/lib/config";
-// import { Button } from "./button";
-
-// export default function AddFavorite({ productID }: { productID: number }) {
-//   const handleClick = async () => {
-//     if (!productID) return console.error("Missing productID");
-
-//     const response = await fetch(`${API_URL}/api/favorites`, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ product_id: productID }),
-//     });
-
-//     if (!response.ok) {
-//       console.error("Failed to add favorite");
-//     } else {
-//       console.log("Added to favorites:", productID);
-//     }
-//   };
-
-//   return (
-//     <Button className="w-fit group" onClick={handleClick}>
-//       <Star
-//         className="transition-transform duration-1000 group-hover:scale-110 group-hover:rotate-[360deg]"
-//         fill="white"
-//       />
-//     </Button>
-//   );
-// }
-
 "use client";
 import confetti from "canvas-confetti";
 import { Star } from "lucide-react";
