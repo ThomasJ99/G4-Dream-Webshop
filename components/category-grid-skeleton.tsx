5bf62282d4db1a556511631b6ba5e61e3fa5e323
ThomasJ99 <xmaslink1999@gmail.com>
2026-04-02 08:41:16 +0200

    Add dimensions to category skeleton, fixed map to not need category fetch

--- components/category-grid-skeleton.tsx
+++ components/category-grid-skeleton.tsx
@@ -1,37 +1,33 @@
-import { getCategories } from "@/lib/db/categories-db";
 import { Skeleton } from "./ui/skeleton";
 
 export default async function CategoryGrid() {
-  const categories = await getCategories();
-
-  const array = categories.splice(0, 3);
   return (
     <section className="py-16">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between mb-8">
           {/* h2 skeleton */}
-          <Skeleton className="font-serif text-2xl sm:text-3xl" />
+          <Skeleton className="h-10 w-50 bg-muted" />
 
           {/* Tiny view all link skeleton */}
-          <Skeleton className="flex items-center gap-1 text-sm text-blue-600 hover:text-blue-700 transition-colors" />
+          <Skeleton className="h-4 w-16" />
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-          {array.map((q) => (
+          {Array.from({ length: 3 }).map((_, i) => (
             <div
-              key={q.id}
-              className="group relative aspect-4/5 rounded-lg overflow-hidden bg-white/95"
+              key={i}
+              className="group relative aspect-4/5 rounded-lg"
             >
               {/* Image skeleton */}
               <Skeleton
                 // Add h/w
-                className="object-cover transition-transform duration-500 group-hover:scale-105"
+                className="bg-muted-foreground absolute inset-0"
               />
 
               <div className="absolute bottom-6 left-6 right-6">
                 {/* Text skeleton on the bottom of the image */}
-                <Skeleton className="text-white text-xl leading-relaxed" />
-                <Skeleton className="text-white/80 text-sm" />
+                <Skeleton className="h-3 w-30" />
+                <Skeleton className="h-3 w-35 mt-2" />
               </div>
             </div>
           ))}
