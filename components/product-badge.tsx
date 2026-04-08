7bd67c98d991f7e25188087c31802387c859dc1a
ThomasJ99 <xmaslink1999@gmail.com>
2026-04-08 14:51:53 +0200

    Add a product badge component with a interface of the desired badges to display

---
+++ components/product-badge.tsx
@@ -0,0 +1,15 @@
+
+// Optional props for each type of information to be displayed
+interface ProductBadgeProps {
+    // Out of stock
+    availabilityStatus?: string
+    // Thinking around 15% or higher, maybe even higher
+    discountPercentage?: number
+    // Show low stock
+    stock?: number
+
+}
+
+export default function ProductBadge() {
+
+}
