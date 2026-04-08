dbc5aacd9f76b07a3bfce17cc2242851f6b5a44d
ThomasJ99 <xmaslink1999@gmail.com>
2026-04-08 15:12:56 +0200

    Add three if statements to check if i should add one of the badges to a product, no check right now if theres multiple being valid at once

--- components/product-badge.tsx
+++ components/product-badge.tsx
@@ -1,15 +1,28 @@
-
 // Optional props for each type of information to be displayed
 interface ProductBadgeProps {
-    // Out of stock
-    availabilityStatus?: string
-    // Thinking around 15% or higher, maybe even higher
-    discountPercentage?: number
-    // Show low stock
-    stock?: number
-
+  // Out of stock
+  availabilityStatus?: string;
+  // Thinking around 15% or higher, maybe even higher
+  discountPercentage?: number;
+  // Show low stock
+  stock?: number;
 }
 
-export default function ProductBadge() {
+export default function ProductBadge({
+  availabilityStatus,
+  discountPercentage,
+  stock,
+}: ProductBadgeProps) {
 
+  // If discound is 15 or above and it isnt undefined
+  if (discountPercentage !== undefined && discountPercentage >= 15) {
+    return <span>🔥 Hot</span>;
+  }
+
+  if (availabilityStatus === "Out of Stock") {
+    return <span>Sold out</span>;
+  }
+
+  if (stock <= 5 && stock !== undefined)
+    return <span>Only {stock} left</span>
 }
