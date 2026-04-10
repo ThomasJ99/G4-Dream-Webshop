09a772c8c66a87bba2f97aa03c57789201fa7549
Alex <olofalexander.larsson@gmail.com>
2026-04-10 10:49:34 +0200

    Fixed toggle add-favorite/remove-favorite.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -13,6 +13,8 @@ async function getProduct(id: number) {
 }
 
 export default async function Favorites() {
+  // TODO: remove delay.
+  // Delay för att se skeleton under dev, MEN produkter tas bort efter 5 sec då.
   await new Promise((resolve) => setTimeout(resolve, 5000));
   // await new Promise(() => {}); // This promise never resolves → loading.tsx stays forever
 
