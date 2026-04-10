68641eaa9e781f0b2907713b52bdfcb89c0d486c
Alex <olofalexander.larsson@gmail.com>
2026-04-10 15:27:48 +0200

    Fixed navbar bug.

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -45,42 +45,43 @@ export default function Navigation({
       ref={menuRef}
       className="sticky z-50 top-0 w-full bg-white/50  shadow-sm"
     >
-      <nav className="flex items-center justify-between backdrop-blur-sm mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-16">
-        <div className="flex-1">
-          <Link
-            className="text-xl font-serif font-bold tracking-tight shrink-0 hover:text-blue-400 transition-colors"
-            href="/"
-          >
-            DreamShop
-          </Link>
-        </div>
+      <div className="w-full h-16 backdrop-blur-sm">
+        <nav className="flex items-center justify-between mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-16">
+          <div className="flex-1">
+            <Link
+              className="text-xl font-serif font-bold tracking-tight shrink-0 hover:text-blue-400 transition-colors"
+              href="/"
+            >
+              DreamShop
+            </Link>
+          </div>
 
-        <ul className="hidden md:flex items-center gap-5">
-          {menuItems.map((item) => (
-            <li key={item.name}>
-              <Link
-                className="p-2 text-md font-semibold hover:text-blue-400 transition-colors"
-                href={item.href}
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+          <ul className="hidden md:flex items-center gap-5">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <Link
+                  className="p-2 text-md font-semibold hover:text-blue-400 transition-colors"
+                  href={item.href}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-        <div className="flex-1 flex items-center justify-end gap-2">
-          <Link
-            className="relative p-2 rounded-sm hover:bg-blue-900/20 hover:text-blue-400 transition-colors"
-            href="/cart"
-          >
-            <div className="absolute -top-1 -right-1 w-4 h-4 bg-blue-400 rounded-full text-white flex items-center justify-center text-xs">
-              {cartItemsLength}
-            </div>
-            <ShoppingCart size={24} />
-          </Link>
+          <div className="flex-1 flex items-center justify-end gap-2">
+            <Link
+              className="relative p-2 rounded-sm hover:bg-blue-900/20 hover:text-blue-400 transition-colors"
+              href="/cart"
+            >
+              <div className="absolute -top-1 -right-1 w-4 h-4 bg-blue-400 rounded-full text-white flex items-center justify-center text-xs">
+                {cartItemsLength}
+              </div>
+              <ShoppingCart size={24} />
+            </Link>
 
-          {/* Leaving old Button here for now. */}
-          {/* <div className="md:hidden">
+            {/* Leaving old Button here for now. */}
+            {/* <div className="md:hidden">
             <button
               className="cursor-pointer p-2 rounded-sm hover:bg-blue-900/20 hover:text-blue-400 transition-colors"
               type="button"
@@ -90,47 +91,48 @@ export default function Navigation({
             </button>
           </div> */}
 
-          {/* New BUTTON */}
-          <div className="md:hidden">
-            <button
-              className="cursor-pointer p-2 rounded-sm w-10 h-10 flex hover:bg-blue-900/20 hover:text-blue-400 transition-colors focus:outline-none"
-              type="button"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <div
-                className={`flex flex-col items-center justify-center w-6 h-6`}
+            {/* New BUTTON */}
+            <div className="md:hidden">
+              <button
+                className="cursor-pointer p-2 rounded-sm w-10 h-10 flex hover:bg-blue-900/20 hover:text-blue-400 transition-colors focus:outline-none"
+                type="button"
+                onClick={() => setIsOpen(!isOpen)}
               >
-                {/* Top Bar Wrapper */}
                 <div
-                  className={`w-full transition-transform ease-in-out ${isOpen ? "duration-400 delay-100 translate-y-[2px]" : "duration-400 translate-y-0 delay-300"}`}
+                  className={`flex flex-col items-center justify-center w-6 h-6`}
                 >
+                  {/* Top Bar Wrapper */}
                   <div
-                    className={`bg-current h-[2px] w-full transition-transform duration-400 ease-in-out ${isOpen ? "duration-400 rotate-45 delay-400" : "duration-400 rotate-0 delay-0"}`}
-                  />
-                </div>
+                    className={`w-full transition-transform ease-in-out ${isOpen ? "duration-400 delay-100 translate-y-[2px]" : "duration-400 translate-y-0 delay-300"}`}
+                  >
+                    <div
+                      className={`bg-current h-[2px] w-full transition-transform duration-400 ease-in-out ${isOpen ? "duration-400 rotate-45 delay-400" : "duration-400 rotate-0 delay-0"}`}
+                    />
+                  </div>
 
-                {/* Middle Bar */}
-                <div
-                  className={`w-full transition-[margin] duration-500 ${isOpen ? "my-0" : "my-2 delay-300"}`}
-                >
+                  {/* Middle Bar */}
                   <div
-                    className={`bg-current h-[2px] w-full transition-transform ease-in-out ${isOpen ? "duration-300 scale-x-0" : "duration-300 delay-400 scale-x-100"}`}
-                  />
-                </div>
+                    className={`w-full transition-[margin] duration-500 ${isOpen ? "my-0" : "my-2 delay-300"}`}
+                  >
+                    <div
+                      className={`bg-current h-[2px] w-full transition-transform ease-in-out ${isOpen ? "duration-300 scale-x-0" : "duration-300 delay-400 scale-x-100"}`}
+                    />
+                  </div>
 
-                {/* Bottom Bar Wrapper */}
-                <div
-                  className={`w-full transition-transform ease-in-out ${isOpen ? "duration-400 delay-100 -translate-y-[2px]" : "duration-400 translate-y-0 delay-300"}`}
-                >
+                  {/* Bottom Bar Wrapper */}
                   <div
-                    className={`bg-current h-[2px] w-full transition-transform ease-in-out ${isOpen ? "duration-400 -rotate-45 delay-400" : "duration-400 rotate-0 delay-0"}`}
-                  />
+                    className={`w-full transition-transform ease-in-out ${isOpen ? "duration-400 delay-100 -translate-y-[2px]" : "duration-400 translate-y-0 delay-300"}`}
+                  >
+                    <div
+                      className={`bg-current h-[2px] w-full transition-transform ease-in-out ${isOpen ? "duration-400 -rotate-45 delay-400" : "duration-400 rotate-0 delay-0"}`}
+                    />
+                  </div>
                 </div>
-              </div>
-            </button>
+              </button>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
 
       <AnimatePresence>
         {isOpen && (
