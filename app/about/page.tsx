836a50180560a989610294e0a3fff317b303aa63
ThomasJ99 <xmaslink1999@gmail.com>
2026-04-14 11:52:23 +0200

    Fix about size to match hero close enough, adjusted some text styling and changed Link to be a shadcn component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,58 +1,58 @@
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default async function About() {
   // TODO: REMOVE THIS LINE OF CODE LATER, GOOD TO TEST SKELETONS
   // await new Promise((resolve) => setTimeout(resolve, 2000))
   return (
-    <article className="relative min-h-[70vh] flex items-center">
-        {/* Version 2: Background image with text on top */}
-        {/* Background Image */}
-        <Image
-          src="https://images.unsplash.com/photo-1698858212917-9dbd472abecb?w=1800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZyZWUlMjBpbWFnZXMlMjBzdG9ja2hvbG18ZW58MHx8MHx8fDA%3D"
-          alt="Our story - Image of Stockholm City, Gamla Stan featuring a prominent church"
-          fill
-          className="object-cover"
-          priority
-        />
+    <article className="relative min-h-[50vh] sm:min-h-[70vh] flex items-center">
+      {/* Version 2: Background image with text on top */}
+      {/* Background Image */}
+      <Image
+        src="https://images.unsplash.com/photo-1698858212917-9dbd472abecb?w=1920&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZyZWUlMjBpbWFnZXMlMjBzdG9ja2hvbG18ZW58MHx8MHx8fDA%3D"
+        alt="Our story - Image of Stockholm City, Gamla Stan featuring a prominent church"
+        fill
+        className="object-cover"
+        priority
+      />
 
-        {/* Linear overlay — dark at bottom so text is more readable */}
-        <div className="absolute inset-0 bg-linear-to-t from-black/80 to-transparent" />
+      {/* Linear overlay — dark at bottom so text is more readable */}
+      <div className="absolute inset-0 bg-linear-to-r from-black/80 via-black/30 to-transparent" />
 
-        {/* Text content */}
-        <section className="relative z-9 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 w-full">
-          <div className="max-w-2xl space-y-6 text-shadow-md">
-            <span className="text-sm text-white uppercase tracking-widest">
-              Our Story
-            </span>
+      {/* Text content */}
+      <section className="relative z-9 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-20 lg:py-32 w-full">
+        <div className="max-w-2xl space-y-6 text-shadow-md text-white">
+          <span className="text-md uppercase font-semibold tracking-widest">
+            Our Story
+          </span>
 
-            <h1 className="font-serif text-4xl sm:text-5xl text-white text-pretty">
-              A store built around you
-            </h1>
+          <h1 className="font-serif text-4xl sm:text-5xl font-semibold text-pretty tracking-tight">
+            A store built around you
+          </h1>
 
-            <p className="text-white leading-relaxed">
-              At DreamShop, we believe shopping should be simple and enjoyable -
-              everything you need, all in one place. From electronics and
-              furniture to beauty and fashion, our curated selection brings
-              together the best products across every category.
-            </p>
+          <p className="leading-relaxed">
+            At DreamShop, we believe shopping should be simple and enjoyable -
+            everything you need, all in one place. From electronics and
+            furniture to beauty and fashion, our curated selection brings
+            together the best products across every category.
+          </p>
 
-            <p className="text-white leading-relaxed">
-              We partner with trusted brands and sellers to make sure every
-              product meets our standards — so you can shop with confidence.
-            </p>
+          <p className="leading-relaxed">
+            We partner with trusted brands and sellers to make sure every
+            product meets our standards — so you can shop with confidence.
+          </p>
 
-            <Link
-              href="/products"
-              className="inline-flex items-center gap-4 border border-black bg-black text-white py-2.5 px-5 rounded-lg hover:bg-transparent hover:border-white transition-colors"
-            >
+          <Link href="/products" className="inline-flex items-center gap-4">
+            <Button type="button" size={"lg"}>
               Discover Our Collection
               <ArrowRight className="h-4 w-4" />
-            </Link>
-          </div>
-        </section>
-      </article>
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </article>
   );
 }
 
